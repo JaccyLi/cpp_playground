b5e6a11e7f94cc1c7b315f703d88e8ebc4ccfce6
suo.li <suo.li@microfun.com>
2021-07-09 20:06:05 +0800

    update

--- run_src.sh
+++ run_src.sh
@@ -5,7 +5,12 @@ out=$(echo $(basename $src) | awk -F'.' '{print $1}')
 
 echo out=$out
 
-if [[ -f $(dirname $src)/${out}.out ]] || [[ -n $2 ]]
+if [[ ! -f $(dirname $src)/${out}.out ]]
+then
+    g++ -o $(dirname $src)/${out}.out $src
+fi
+
+if [[ -f $(dirname $src)/${out}.out ]] && [[ -n $2 ]]
 then
     g++ -o $(dirname $src)/${out}.out $src
     ./$(dirname $src)/${out}.out
